7739dbe420fe319370ccbd17a7d63c4aeb6b0f53
Vollga <36159821+Vollga@users.noreply.github.com>
2018-02-12 17:20:38 +0100

    2

---
+++ Week 2/Assets/Colourchange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colourchange : MonoBehaviour {
+
+	// Use this for initialization
+
+	public MeshRenderer meshRenderer;
+	public Color colour;
+
+	void Start () {
+		meshRenderer = GetComponent<MeshRenderer>
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("up"))
+			meshRenderer.material.SetColor (colour);
+	}
+}
